042e94132fe93fb4ed28edf25769f9c40d0b538f
ProjectTL12345 <Project33890@>
2020-12-01 20:14:43 +0900

    init

--- src/main/groovy/org/projecttl/api/createitem/commands/ExampleItem.groovy
+++ src/main/groovy/org/projecttl/api/createitem/commands/ExampleItem.groovy
@@ -8,7 +8,6 @@ import org.bukkit.command.CommandExecutor
 import org.bukkit.command.CommandSender
 import org.bukkit.entity.Player
 import org.bukkit.inventory.Inventory
-import org.projecttl.api.createitem.utils.CreateItem
 import org.projecttl.api.inventorygui.utils.AddGuiItem
 
 class ExampleItem extends AddGuiItem implements CommandExecutor {
